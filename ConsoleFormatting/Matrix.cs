0c17d5a4dda4114a2d4723e1cbf2e8de54a984f4
PortalArm <evgenii_kim2010@mail.ru>
2019-12-24 12:25:22 +0600

    Added "Matrix" class Added methods' signatures

---
+++ ConsoleFormatting/Matrix.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFormatting
+{
+    /// <summary>
+    /// This class implements two-dimensional matrix.
+    /// </summary>
+    class Matrix
+    {
+
+        /// <summary>
+        /// Describes the random numbers boundaries.
+        /// </summary>
+        private readonly int _rand = 32;
+
+        /// <summary>
+        /// Variable for storing the matrix itself.
+        /// </summary>
+        private double[,] val;
+
+
+
+        /// <summary>
+        /// Indexer for easy access to the matrix.
+        /// </summary>
+        /// <param name="i">Row index</param>
+        /// <param name="j">Column index</param>
+        /// <returns></returns>
+        public double this[int i,int j]
+        {
+            get { return val[i, j]; }
+            set { val[i, j] = value; }
+        }
+
+        /// <summary>
+        /// Creates n x m matrix.
+        /// </summary>
+        /// <param name="n">Rows count</param>
+        /// <param name="m">Columns count</param>
+        public Matrix(int n, int m)
+        {
+            val = new double[n, m];
+        }
+
+        /// <summary>
+        /// Fills the matrix with random values in range [-_rand, _rand)
+        /// </summary>
+        public void FillRandom()
+        {
+            
+        }
+
+        /// <summary>
+        /// Prints the matrix to console output.
+        /// </summary>
+        public void Print()
+        {
+
+        }
+
+        /// <summary>
+        /// Adds two matrices.
+        /// </summary>
+        /// <param name="a">First matrix</param>
+        /// <param name="b">Second matrix</param>
+        /// <returns></returns>
+        static public Matrix Add(Matrix a, Matrix b)
+        {
+            return null;
+
+            
+        }
+
+        /// <summary>
+        /// Multiplies two matrices.
+        /// </summary>
+        /// <param name="a">First matrix</param>
+        /// <param name="b">Second matrix</param>
+        /// <returns></returns>
+        static public Matrix Multiply(Matrix a, Matrix b)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Overloaded operator for matrices addition.
+        /// </summary>
+        /// <param name="left">Left operand</param>
+        /// <param name="right">Right operand</param>
+        /// <returns></returns>
+        static public Matrix operator +(Matrix left, Matrix right)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Overloaded operator for matrices multiplication.
+        /// </summary>
+        /// <param name="left">Left operand</param>
+        /// <param name="right">Right operand</param>
+        /// <returns></returns>
+        static public Matrix operator *(Matrix left, Matrix right)
+        {
+            return null;
+        }
+
+
+    }
+
+}
