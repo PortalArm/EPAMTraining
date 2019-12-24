f03b62ee5eb7c91c04c5dd1a4874d4b31f65bfcf
PortalArm <evgenii_kim2010@mail.ru>
2019-12-24 10:57:15 +0600

    Added console compilation files

---
+++ ConsoleCompiling/Compile.bat
@@ -0,0 +1,3 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe Program.cs Logic.cs
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -target:library Program.cs Logic.cs
+pause
---
+++ ConsoleCompiling/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic logic = new Logic();
+            Console.WriteLine(logic.Method());
+            Console.ReadLine();
+        }
+    }
+}
