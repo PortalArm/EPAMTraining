c76ab7248eb23fa8b46ddb3cab3373e7e8ce4adc
PortalArm <evgenii_kim2010@mail.ru>
2020-01-03 01:52:34 +0600

    Add unfinished calculator project

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+
+        public void Calculate()
+        {
+
+        }
+
+    }
+}
