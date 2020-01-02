2a00fc3ed74a1524f26baea12fed4c22d1e52a7a
PortalArm <evgenii_kim2010@mail.ru>
2020-01-03 01:52:56 +0600

    Added unfinished GCD project

---
+++ GCDCalculation/GCDHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCDCalculation
+{
+    public static class GCDHelper
+    {
+        private static readonly Stopwatch _stopwatch = new Stopwatch();
+
+        /// <summary>
+        /// Вычисление НОД для двух чисел
+        /// </summary>
+        /// <param name="a">Первое число</param>
+        /// <param name="b">Второе число</param>
+        /// <returns>НОД двух чисел</returns>
+        public static long GCD(long a,long b)
+        {
+            while(b != 0)
+                (a, b) = (b, a % b);
+
+            return a;
+        }
+        public static long GCD(params long[] values)
+        {
+
+        }
+
+    }
+}
