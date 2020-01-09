bd9b1805b4a842c05440ba90501c8b4be1b2818d
PortalArm <evgenii_kim2010@mail.ru>
2020-01-09 15:27:52 +0600

    Changed name of Form1 to MainForm

--- ComputerraWF/Program.cs
+++ ComputerraWF/Program.cs
@@ -18,7 +18,7 @@ namespace ComputerraWF
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
