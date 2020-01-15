ba3359608a5a372f4992f2bcb17a89ca2d04e6d4
PortalArm <evgenii_kim2010@mail.ru>
2020-01-16 02:38:43 +0600

    Add homework 4

---
+++ HomeWork04/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = null;
+            double[] doubles;
+
+            //Проверка ввода: Если введены не числа, количество чисел не 3, то выведется ошибка
+            //Далее, если действительно введены три числа, во второй части условия проверяется, может ли быть создан треугольник с такими длинами. 
+            //Если все проверки соблюдены, выводится объект (вся информация о нем, переопределен метод ToString)
+            while (!InputHandler.GetArrayInputFromConsoleOneLine("Введите 3 числа", out doubles, expectedSize: 3, errorMessage: (e) => $"Вы ввели неверные числа ({e.Message})") ||
+                InputHandler.Catch<ArgumentException>(() => { triangle = new Triangle(doubles[0], doubles[1], doubles[2]); }, (e) => Console.WriteLine(e.Message)));
+            Console.WriteLine(triangle);
+            Console.ReadKey();
+        }
+    }
+
+    public static class InputHandler
+    {
+
+        private static Action<string> _logger = Console.WriteLine;
+
+        //Метод для установки действия, которое будет выполняться для текста
+        public static void SetLogger(Action<string> logger) => _logger = logger;
+
+        //Метод для "компактной" замены блока try-catch-finally
+        public static bool Catch<T>(Action _try, Action<T> _catch = null, Action _finally = null) where T : Exception
+        {
+            bool error = false;
+
+            try { _try(); }
+            catch (T e)
+            {
+                error = true;
+                _catch?.Invoke(e);
+            }
+            finally { _finally?.Invoke(); }
+
+            return error;
+        }
+        public static bool CatchAny(Action _try, Action<Exception> _catch = null, Action _finally = null) => Catch(_try, _catch, _finally);
+
+        //Метод для получения входных данных, которые представляют массив определенного типа T, из консоли.
+        public static bool GetArrayInputFromConsoleOneLine<T>(string message, out T[] output, int expectedSize = 0, Func<Exception, string> errorMessage = null, char[] delimiter = null)
+        {
+            _logger(message);
+            char[] realDelim = delimiter ?? new[] { ' ' };
+            output = default;
+            T[] outs = null;
+            bool result = CatchAny(
+                _try:   () => outs = Console.ReadLine().Split(realDelim).Select(w => (T)Convert.ChangeType(w, typeof(T))).ToArray(),
+                _catch: (e) => _logger(errorMessage?.Invoke(e))
+                );
+
+            if (result)
+                return false;
+
+            if (expectedSize != 0 && outs.Length != expectedSize)
+            {
+                _logger(errorMessage?.Invoke(new InvalidOperationException($"Ожидалась размерность {expectedSize}")));
+                return false;
+            }
+
+            output = outs;
+            return true;
+        }
+
+        //Метод для получения входных данных, которые представляют значение типа T, из консоли.
+        public static bool GetInputFromConsoleOneLine<T>(string message, out T output, Func<Exception, string> errorMessage = null)
+        {
+            _logger(message);
+            output = default;
+            T _out = default;
+
+            bool result = CatchAny(
+                _try:   () => _out = (T)Convert.ChangeType(Console.ReadLine(), typeof(T)),
+                _catch: (e) => _logger(errorMessage?.Invoke(e))
+                );
+
+            if (result)
+                return false;
+
+            output = _out;
+            return true;
+        }
+    }
+}
---
+++ HomeWork04/Triangle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork04
+{
+    public enum AngleType { Degrees, Radians }
+    class Triangle
+    {
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double C { get; private set; }
+
+        private Dictionary<double, double> _angles = new Dictionary<double, double>();
+        public double Perimeter { get; }
+        public double Area { get; }
+        public Triangle(double a, double b, double c)
+        {
+            if (a >= b + c || b >= a + c || c >= a + b || a <= 0 || b <= 0 || c <= 0)
+                throw new ArgumentException("Треугольник с введенными длинами должен существовать");
+
+            A = a; B = b; C = c;
+            Perimeter = A + B + C;
+            
+            //Нахождение площади
+            double halfp = Perimeter / 2;
+            Area = Math.Sqrt(halfp * (halfp - A) * (halfp - B) * (halfp - C));
+
+            //Отображение углов к соответствующим сторонам
+            _angles.Add(A, GetAngle(A, B, C));
+            _angles.Add(B, GetAngle(B, A, C));
+            _angles.Add(C, GetAngle(C, B, A));
+        }
+
+        // Вспомогательный приватный метод, который возвращает значение угла (в радианах), противолежащего стороне mainSide, с другими сторонами a и b.
+        private double GetAngle(double mainSide, double a, double b) => Math.Acos((a * a + b * b - mainSide * mainSide) / (2 * a * b));
+
+        //Возвращает значение угла, противолежащего стороне sideValue (в градусах/радианах, в зависимости от второго параметра).
+        public double GetAngle(double sideValue, AngleType type = AngleType.Degrees)
+        {
+            if (!_angles.TryGetValue(sideValue, out double angle))
+                throw new ArgumentException($"Не найдена сторона с длиной {sideValue}", "sideValue");
+
+            switch (type)
+            {
+                //т.к. Math.Acos возвращает значение в радианах, для перевода в градусы достаточно умножить на 180 и разделить на Пи
+                case AngleType.Degrees:
+                    return angle * 180d / Math.PI;
+                case AngleType.Radians:
+                    return angle;
+                default:
+                    throw new ArgumentException($"Не найден тип угла", "type");
+            }
+        }
+        public override string ToString() => string.Format(@"Треугольник со сторонами:                  {0,10} {1,10} {2,10}
+Угол, противолежащий стороне (в радианах): {3,10:0.00000} {4,10:0.00000} {5,10:0.00000}
+Угол, противолежащий стороне (в градусах): {6,10:0.00000} {7,10:0.00000} {8,10:0.00000}
+Периметр: {9}
+Площадь: {10}", A, B, C, 
+            GetAngle(A, AngleType.Radians), GetAngle(B, AngleType.Radians), GetAngle(C, AngleType.Radians), 
+            GetAngle(A), GetAngle(B), GetAngle(C),
+            Perimeter, Area);
+    }
+}
